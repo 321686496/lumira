ff01a7b0778dbdefd3df2cbe31cc8de5ce4df28d
hh <hh@qq.com>
2026-07-24 11:39:14 +0800

    feat(splash): add brand color halo behind logo

--- lumira_app_flutter/test/features/splash/splash_page_test.dart
+++ lumira_app_flutter/test/features/splash/splash_page_test.dart
@@ -7,6 +7,7 @@ import 'package:lumira_app_flutter/core/router/route_names.dart';
 import 'package:lumira_app_flutter/core/theme/theme_controller.dart';
 import 'package:lumira_app_flutter/core/theme/theme_tokens.dart';
 import 'package:lumira_app_flutter/features/splash/pages/splash_page.dart';
+import 'package:lumira_app_flutter/shared/widgets/brand/lumira_logo.dart';
 
 Widget _wrapWithRouter(Widget child, {ThemeKey theme = ThemeKey.warmWhite}) {
   final router = GoRouter(
@@ -36,13 +37,15 @@ Widget _wrapWithRouter(Widget child, {ThemeKey theme = ThemeKey.warmWhite}) {
 }
 
 void main() {
-  testWidgets('SplashPage renders logo + title + caption', (tester) async {
+  testWidgets('SplashPage renders logo + title + caption + brand halo', (tester) async {
     await tester.pumpWidget(_wrapWithRouter(const SplashPage()));
     await tester.pump(const Duration(milliseconds: 100));
 
     expect(find.text('如画 Lumira'), findsOneWidget);
     expect(find.text('如你所见，皆成画卷'), findsOneWidget);
-    expect(find.byIcon(Icons.camera_outlined), findsOneWidget);
+    expect(find.byType(LumiraLogo), findsOneWidget);
+    // 主题色光晕：用 Stack + Container(circle + RadialGradient)
+    expect(find.byType(Stack), findsWidgets);
   });
 
   testWidgets('SplashPage uses tokens.canvas as background', (tester) async {
