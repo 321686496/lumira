9b07cd69d7cb9675e3fb5313a85321883ed6f118
hh <321686496@qq.com>
2026-08-22 12:51:02 +0800

    feat(camera): OHOS 传感器级白平衡 (CameraKit)

--- lumira_app_flutter/packages/camerawesome_ohos/lib/pigeon.dart
+++ lumira_app_flutter/packages/camerawesome_ohos/lib/pigeon.dart
@@ -915,6 +915,28 @@ class CameraInterface {
     }
   }
 
+  Future<void> setWhiteBalance(String arg_mode, int? arg_k) async {
+    final BasicMessageChannel<Object?> channel = BasicMessageChannel<Object?>(
+        'dev.flutter.pigeon.camerawesome.CameraInterface.setWhiteBalance', codec,
+        binaryMessenger: _binaryMessenger);
+    final List<Object?>? replyList =
+        await channel.send(<Object?>[arg_mode, arg_k]) as List<Object?>?;
+    if (replyList == null) {
+      throw PlatformException(
+        code: 'channel-error',
+        message: 'Unable to establish connection on channel.',
+      );
+    } else if (replyList.length > 1) {
+      throw PlatformException(
+        code: replyList[0]! as String,
+        message: replyList[1] as String?,
+        details: replyList[2],
+      );
+    } else {
+      return;
+    }
+  }
+
   Future<void> handleAutoFocus() async {
     final BasicMessageChannel<Object?> channel = BasicMessageChannel<Object?>(
         'dev.flutter.pigeon.camerawesome.CameraInterface.handleAutoFocus', codec,
